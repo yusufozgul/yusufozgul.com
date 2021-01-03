2f7440a922828a0c97ecaab925606a59bbbb3f3b
Yusuf Özgül <yozgul@icloud.com>
2021-01-03 17:23:44 +0300

    Add plugins, site detail

--- Package.swift
+++ Package.swift
@@ -11,12 +11,27 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0")
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(url: "https://github.com/alexito4/ReadingTimePublishPlugin", from: "0.1.0"),
+        .package(url: "https://github.com/insidegui/TwitterPublishPlugin.git", from: "0.1.0"),
+        .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0"),
+        .package(url: "https://github.com/finestructure/ImageAttributesPublishPlugin", from: "0.1.1"),
+        .package(name: "LinkAttributesPublishPlugin", url: "https://github.com/cweinberger/LinkAttributesPublishPlugin", from: "1.0.0"),
+        .package(url: "https://github.com/thomaslupo/GistPublishPlugin", from: "0.1.0"),
+        .package(url: "https://github.com/wacumov/VerifyResourcesExistPublishPlugin", from: "0.1.0"),
+        .package(name: "yusufozgul.comTheme", path: "./yusufozgul.comTheme/")
     ],
     targets: [
         .target(
             name: "YusufozgulCom",
-            dependencies: ["Publish"]
+            dependencies: ["Publish", "ReadingTimePublishPlugin",
+                           "TwitterPublishPlugin",
+                           "SplashPublishPlugin",
+                           "ImageAttributesPublishPlugin",
+                           "LinkAttributesPublishPlugin",
+                           "GistPublishPlugin",
+                           "VerifyResourcesExistPublishPlugin",
+                           "yusufozgul.comTheme"]
         )
     ]
 )
--- Sources/YusufozgulCom/main.swift
+++ Sources/YusufozgulCom/main.swift
@@ -1,12 +1,23 @@
 import Foundation
 import Publish
 import Plot
+import ReadingTimePublishPlugin
+import TwitterPublishPlugin
+import SplashPublishPlugin
+import ImageAttributesPublishPlugin
+import LinkAttributesPublishPlugin
+import GistPublishPlugin
+import VerifyResourcesExistPublishPlugin
+import yusufozgul_comTheme
 
 // This type acts as the configuration for your website.
 struct YusufozgulCom: Website {
     enum SectionID: String, WebsiteSectionID {
-        // Add the sections that you want your website to contain here:
-        case posts
+        case home
+        case about
+        case contact
+        case projects
+        case blogs
     }
 
     struct ItemMetadata: WebsiteItemMetadata {
@@ -14,12 +25,30 @@ struct YusufozgulCom: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
-    var name = "YusufozgulCom"
-    var description = "A description of YusufozgulCom"
-    var language: Language { .english }
+    var url = URL(string: "https://yusufozgul.com")!
+    var name = "Yusuf Özgül | Blog | Resume | Portfolio"
+    var description = "Kişisel blog, yazılım hakkında paylaşımlar ve güncel projelerim."
+    var language: Language { .turkish }
     var imagePath: Path? { nil }
+    var favicon: Favicon? = .init(path: "/upload-images/favicon.png", type: "image/png")
+    
+    init() {
+        LoadData.load()
+    }
 }
 
 // This will generate your website using the built-in Foundation theme:
-try YusufozgulCom().publish(withTheme: .foundation)
+try YusufozgulCom().publish(using: [
+    .installPlugin(.twitter()),
+    .installPlugin(.gist()),
+    .installPlugin(.linkAttributes()),
+    .installPlugin(.imageAttributes()),
+    .installPlugin(.splash(withClassPrefix: "")),
+    .addMarkdownFiles(),
+    .copyFiles(at: "/Resources/upload-images", to: "/upload-images"),
+    .installPlugin(.readingTime(wordsPerMinute: 40)),
+    .generateHTML(withTheme: .yusufozgulcom),
+    .generateSiteMap(),
+    .installPlugin(.verifyResourcesExist()),
+    .deploy(using: .git("https://yusuf.ozgul@github.com/yusufozgul/YusufOzgul-Deploys"))
+])
